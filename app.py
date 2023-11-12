9f57d453d91d8bf65b335ba111c6233f7fd1b842
Oscar_AGS <122835599+osaradahir@users.noreply.github.com>
2023-11-12 13:47:41 -0600

    Update app.py

--- app.py
+++ app.py
@@ -11,3 +11,23 @@ def index():
 @app.route("/buscar")
 def buscar():
     return render_template('buscar.html')
+
+@app.route("/prueba")
+def prueba():
+    return render_template('prueba.html')
+
+@app.route("/ver")
+def ver():
+    return render_template('ver.html')
+
+@app.route("/insertar")
+def insertar():
+    return render_template('insertar.html')
+
+@app.route("/actualizar")
+def actualizar():
+    return render_template('actualizar.html')
+
+@app.route("/borrar")
+def borrar():
+    return render_template('borrar.html')
